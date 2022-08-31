858ac451bfe4a981bc62a3bc2847c0db3c792546
Xue Yu <xueyublue@gmail.com>
2022-08-31 15:44:53 +0800

    Test with EmbeddedKafka

--- kafka-demo-producer/src/test/java/sg/darren/kafka/controller/LibraryEventControllerTest.java
+++ kafka-demo-producer/src/test/java/sg/darren/kafka/controller/LibraryEventControllerTest.java
@@ -6,12 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 import sg.darren.kafka.domain.Book;
 import sg.darren.kafka.domain.LibraryEvent;
 
 import java.util.Date;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"}
+)
 class LibraryEventControllerTest {
 
     @Autowired
