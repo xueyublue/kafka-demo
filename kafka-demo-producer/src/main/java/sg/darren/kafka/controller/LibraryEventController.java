29ee66f40dce59c1e0490f88e9f515f93079a7ce
Xue Yu <xueyublue@gmail.com>
2022-08-31 17:35:59 +0800

    PUT

--- kafka-demo-producer/src/main/java/sg/darren/kafka/controller/LibraryEventController.java
+++ kafka-demo-producer/src/main/java/sg/darren/kafka/controller/LibraryEventController.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sg.darren.kafka.domain.LibraryEvent;
 import sg.darren.kafka.domain.LibraryEventType;
 import sg.darren.kafka.producer.LibraryEventProducer;
@@ -17,11 +14,12 @@ import java.util.Date;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/library-event")
 public class LibraryEventController {
 
     private final LibraryEventProducer libraryEventProducer;
 
-    @PostMapping("/v1/library-event")
+    @PostMapping
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
         libraryEvent.setId(new Date().getTime());
         libraryEvent.setLibraryEventType(LibraryEventType.NEW);
@@ -29,9 +27,11 @@ public class LibraryEventController {
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 
-    @PutMapping("/v1/library-event")
-    public ResponseEntity<LibraryEvent> putLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
-        libraryEvent.setId(new Date().getTime());
+    @PutMapping
+    public ResponseEntity<Object> putLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+        if (libraryEvent.getId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please input Event ID.");
+        }
         libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
         libraryEventProducer.sendLibraryEvent2(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
