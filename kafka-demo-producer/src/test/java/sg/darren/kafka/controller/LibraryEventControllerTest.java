dda5f22f9f4a08166d4a87fff0ce5884267d44e4
Xue Yu <xueyublue@gmail.com>
2022-08-31 15:32:01 +0800

    Added initial unit test case

---
+++ kafka-demo-producer/src/test/java/sg/darren/kafka/controller/LibraryEventControllerTest.java
@@ -0,0 +1,42 @@
+package sg.darren.kafka.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import sg.darren.kafka.domain.Book;
+import sg.darren.kafka.domain.LibraryEvent;
+
+import java.util.Date;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LibraryEventControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void postLibraryEvent() {
+        // given
+        LibraryEvent le = LibraryEvent.builder()
+                .id(null)
+                .book(Book.builder()
+                        .id(new Date().getTime())
+                        .name("Kafka Crash Course")
+                        .author("Udemy")
+                        .build())
+                .build();
+        HttpHeaders hh = new HttpHeaders();
+        hh.set("Content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> he = new HttpEntity<>(le, hh);
+
+        // when
+        ResponseEntity<LibraryEvent> re = testRestTemplate.exchange("/v1/library-event", HttpMethod.POST, he, LibraryEvent.class);
+
+        // then
+        Assertions.assertEquals(HttpStatus.CREATED, re.getStatusCode());
+    }
+
+}
