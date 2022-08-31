a5418aecd682bf0d7059cafc826a63bc8bae9ed4
Xue Yu <xueyublue@gmail.com>
2022-08-31 17:53:34 +0800

    Initialc commit - consumer project

---
+++ kafka-demo-consumer/src/main/java/sg/darren/kafka/KafkaDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package sg.darren.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoConsumerApplication.class, args);
+	}
+
+}
---
+++ kafka-demo-consumer/src/test/java/sg/darren/kafka/KafkaDemoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.darren.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
